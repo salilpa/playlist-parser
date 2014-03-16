10db08de6108583526350acc4b930fde8ba2028f
salil <salilpa007@gmail.com>
2014-03-16 17:45:48 +0530

    deleting travis

--- playlist.py
+++ playlist.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 
 #returns a page with links to all websites
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["POST", "GET"])
 def index():
-    station = json.loads(request.form["data"])
-    return ",".join(video_text_from_url(station["url"], station["settings"], station["soup_path_for_list"], station["soup_path_for_keyword"]))
+    if request.method == 'POST':
+        station = json.loads(request.form["data"])
+        return ",".join(video_text_from_url(station["url"], station["settings"], station["soup_path_for_list"], station["soup_path_for_keyword"]))
+    else:
+        return "All is well"
