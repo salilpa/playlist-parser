1cfb8f54bd05b97b845a73cd6e8f3caed2f73a54
salil <salilpa007@gmail.com>
2014-03-29 20:17:09 +0530

    Adding server side functions for video search

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from apiclient.discovery import build
+from settings import DEVELOPER_KEY
 
 
 #returns a list of keywords from the url given using the soup paths
@@ -19,3 +21,15 @@ def video_text_from_url(url, settings, soup_path_for_list, soup_path_for_keyword
         return keyword_list
     else:
         return []
+
+
+def get_video_from_keyword(keyword):
+    video = {}
+    youtube = build("youtube", "v3", developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(q=keyword, part="id,snippet", maxResults="1", type="video").execute()
+    for search_result in search_response.get("items", []):
+        video["videoId"] = search_result["id"]["videoId"]
+        video["thumbnail"] = search_result["snippet"]["thumbnails"]["medium"]["url"]
+        video["title"] = search_result["snippet"]["title"]
+        video["description"] = search_result["snippet"]["description"]
+    return video
--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ from pymongo import MongoClient
 
 db = MongoClient(environ.get("DB_URL"))["playlist"]
 PER_PAGE = 15
+DEVELOPER_KEY = environ.get("DEVELOPER_KEY")
