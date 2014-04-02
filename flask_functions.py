965936b0fcdf58b8149393df393aa0ff87810e9e
Salil Panikkaveettil <salilpa007@gmail.com>
2014-04-02 19:51:34 +0530

    Adding views for the various functions

--- flask_functions.py
+++ flask_functions.py
@@ -1,18 +1,22 @@
 from settings import PER_PAGE
-from flask import url_for
+from flask import url_for, request
 
-def generate_meta_key_urls(db, attr, value=None, size=4):
-    if value:
-        attr_keys = db.stations.find({"meta." + attr: value})[:size]
-        attr_keys_length = attr_keys.count()
-    else:
-        attr_keys = db.stations.distinct("meta." + attr)[:size]
-        attr_keys_length = len(attr_keys)
+def generate_meta_key_urls(db, attr, size=4):
+    #returns urls for different value of a specific key
+    #eg country returns urls for india and us
+
+    attr_keys = db.stations.distinct("meta." + attr)[:size]
 
     urls = []
-    for i in range(0, attr_keys_length/PER_PAGE + 1):
-        if value:
-            urls.append(url_for(".get_meta_value", meta_key = attr, meta_val=value, page_number=i+1))
-        else:
-            urls.append(url_for(".get_meta_key", meta_key = attr, page_number=i+1))
+    for keys in attr_keys:
+        urls.append({
+            "name": keys,
+            "url" :url_for(".get_meta_value", meta_key = attr, meta_val=keys, page_number=1)
+        })
+
     return urls
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
--- playlist.py
+++ playlist.py
@@ -1,11 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from functions import *
 import json
 from decorator import crossdomain
 from settings import db, PER_PAGE
 from pagination import Pagination
+from flask_functions import *
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 #returns a page with links to all websites
@@ -46,17 +49,33 @@ def get_meta_value(meta_key, meta_val, page_number):
 
 @app.route('/<string:meta_key>/page/<int:page_number>')
 def get_meta_key(meta_key, page_number):
-    stations = db.stations.distinct("meta." + meta_key)
-    count = len(stations)
+    meta_values = generate_meta_key_urls(db, meta_key, page_number*PER_PAGE)
+    count = len(meta_values)
     if count > (page_number - 1)*PER_PAGE:
         #should return list of objects divided by page
-        stations_to_be_displayed = stations[(page_number-1)*PER_PAGE:page_number*PER_PAGE]
+        meta_to_be_displayed = {
+            "name" : meta_key,
+            "urls" : meta_values[(page_number-1)*PER_PAGE:page_number*PER_PAGE]
+        }
         pagination = Pagination(page_number, PER_PAGE, count)
-        return "More than zero documents"
+        return render_template("station_list.html", pagination=pagination, meta_to_be_displayed = meta_to_be_displayed)
     else:
         return "None found"
 
 @app.route('/')
 def index():
+    url_blocks = []
+    url_blocks.append({
+        "name" : "country",
+        "url" : url_for(".get_meta_key", meta_key="country", page_number=1),
+        "sub_urls": generate_meta_key_urls(db, "country")
+    })
+    url_blocks.append({
+        "name" : "language",
+        "url" : url_for(".get_meta_key", meta_key="language", page_number=1),
+        "sub_urls": generate_meta_key_urls(db, "language")
+    })
     #find all meta keys and give urls to it
-    return "dummy index function"
+    return render_template("index.html", url_blocks=url_blocks)
+
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
