afd2290f6a8aabe8396925b624aac00641f355f1
salil <salilpa007@gmail.com>
2014-04-06 23:42:44 +0530

    autoplay in js files adding macros to 4 tiles per page

--- static/js/player.js
+++ static/js/player.js
@@ -17,18 +17,18 @@ function onYouTubeIframeAPIReady() {
         //find the first video id
         videoId: videoIds[getHashFromUrl()],
         events: {
-            //'onReady': onPlayerReady,
+            'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
     });
 }
 
 // 4. The API will call this function when the video player is ready.
-/*
+
 function onPlayerReady(event) {
     event.target.playVideo();
 }
-*/
+
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
