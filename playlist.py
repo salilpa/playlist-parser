31e71d6cebd0d6656c179fc87085475308282945
salil <salilpa007@gmail.com>
2014-05-25 22:23:22 +0530

    creating a false sitemap.xml file

--- playlist.py
+++ playlist.py
@@ -206,18 +206,8 @@ def robots():
 
 @app.route('/sitemap.xml', methods=['GET'])
 def sitemap():
-    """Generate sitemap.xml. Makes a list of urls and date modified."""
-    pages = []
-    ten_days_ago = (datetime.now() - timedelta(days=1)).date().isoformat()
-    # static pages
-    for rule in app.url_map.iter_rules():
-        if "GET" in rule.methods and len(rule.arguments) == 0:
-            pages.append(
-                [rule.rule, ten_days_ago]
-            )
-
-    sitemap_xml = render_template('sitemap.xml', pages=pages)
-    response= make_response(sitemap_xml)
+    sitemap_xml = render_template('sitemap.xml')
+    response = make_response(sitemap_xml)
     response.headers["Content-Type"] = "application/xml"
 
     return response
